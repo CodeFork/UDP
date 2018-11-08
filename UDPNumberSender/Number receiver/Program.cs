d6e7bb128a93a0d96722497fdb658bc90d8f5657
Sebastian Petersen <sebastian@srmail.dk>
2018-11-08 11:12:25 +0100

    Sender receiver.

---
+++ UDPNumberSender/Number receiver/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_reciever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Creates a UdpClient for reading incoming data.
+            UdpClient udpServer = new UdpClient(7011);
+
+            //Creates an IPEndPoint to record the IP Address and port number of the sender.  
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(ip, 7011);
+           
+            try
+            {
+                // Blocks until a message is received on this socket from a remote host (a client).
+                Console.WriteLine("Server is blocked");
+                while (true)
+                {
+                    Byte[] receiveBytes = udpServer.Receive(ref RemoteIpEndPoint);
+                    //Server is now activated");
+
+                    string receivedData = Encoding.ASCII.GetString(receiveBytes);
+
+                    Console.WriteLine(receivedData);
+                    //Console.WriteLine("Received from: " + clientName.ToString() + " " + text.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        
+    }
+}
---
+++ UDPNumberSender/Number sender/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Number_sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("192.168.24.231"); //
+            UdpClient udpClient = new UdpClient("192.168.24.231", 7011);
+
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(ip, 7011); //
+            //udpClient.Connect(RemoteIpEndPoint); //
+
+            Console.Write("Press any key to start...");
+            Console.ReadKey();
+
+            while(true)
+            {
+                Random rnd = new Random();
+                int number = rnd.Next(1, 10);
+
+                Byte[] sendBytes = Encoding.ASCII.GetBytes("Sensor: " + number);
+
+                udpClient.Send(sendBytes, sendBytes.Length); //, RemoteEndPoint);
+
+                Thread.Sleep(100);
+            }
+
+        }
+    }
+}
