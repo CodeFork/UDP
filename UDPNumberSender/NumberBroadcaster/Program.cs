2f29fa3807ceba30841de6ddce2163930dea4a93
Sebastian Petersen <sebastian@srmail.dk>
2018-11-08 14:33:37 +0100

    Broadcaster and receiver.

---
+++ UDPNumberSender/NumberBroadcaster/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NumberBroadcaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient();
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Broadcast, 9876);
+
+            Console.Write("Press any key to start...\n");
+            Console.ReadKey();
+
+            while(true)
+            {
+                Random rnd = new Random();
+                int number = rnd.Next(1, 10);
+
+                Byte[] sendBytes = Encoding.ASCII.GetBytes("Sensor: " + number);
+
+                udpClient.Send(sendBytes, sendBytes.Length, endpoint);
+                
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
---
+++ UDPNumberSender/NumberBroadcasterReceiver/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberBroadcasterReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udp = new UdpClient(9876);
+            IPEndPoint RemoteEndPoint = new IPEndPoint(IPAddress.Any, 9876);
+
+            while (true)
+            {
+                Byte[] receiveBytes = udp.Receive(ref RemoteEndPoint);
+
+                string receivedData = Encoding.ASCII.GetString(receiveBytes);
+
+                Console.WriteLine(receivedData);
+            }
+        }
+
+    }
+}
