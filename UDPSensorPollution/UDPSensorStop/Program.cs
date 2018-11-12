0471dbcce29be5cceaa5f1f84026f3112d860a96
Sebastian Petersen <sebastian@srmail.dk>
2018-11-12 18:22:27 +0100

    Added UDPSensorStop.

---
+++ UDPSensorPollution/UDPSensorStop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPSensorStop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient();
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Broadcast, 11111);
+
+            Console.Write("Press any key to stop the server!\n");
+            Console.ReadKey();
+
+            Byte[] sendBytes = Encoding.ASCII.GetBytes("stop");
+            udpClient.Send(sendBytes, sendBytes.Length, endpoint);
+        }
+    }
+}
